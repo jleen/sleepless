93d51651c2d5f633f41c9a522518be9b56631479
John Leen <jleen@saturnvalley.org>
2020-04-22 12:44:01 -0400

    Caffeinated python

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='sleepless',
+    version='0.1',
+    packages=['sleepless'],
+)
---
+++ sleepless/__init__.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2013-2014 John Leen
+
+import platform
+import subprocess
+import sys
+
+
+def caffeinate():
+    if platform.system() == 'Darwin':
+        from discjockey import pmset
+        pmset.prevent_idle_sleep('Disc Jockey Rip')
+    elif platform.system() == 'Windows':
+        import ctypes
+        ES_CONTINUOUS = 0x80000000
+        ES_SYSTEM_REQUIRED = 0x00000001
+        ctypes.windll.kernel32.SetThreadExecutionState(
+                ES_CONTINUOUS | ES_SYSTEM_REQUIRED)
